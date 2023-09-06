bd41f7988714e9414587d9556acc12eabf493c12
ustaad jana gale <najaajoon0@gail.com>
2023-09-06 11:12:01 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,69 @@
+
+let input=document.querySelector("input");
+
+let option=document.querySelector("option")
+// console.log(option)
+let buttons=document.querySelectorAll("button");
+let reset=0;
+let number="99+"
+// console.log(buttons)
+
+
+
+
+//count funtion
+var counter=0;
+function countClick(){
+    counter=counter+1;
+    input.value=counter
+    // output.innerHTML=counter
+
+}
+//option funtion
+function optionCount(message){
+    option.innerHTML=message;
+}
+
+
+
+
+
+
+//event 
+
+buttons.forEach((btn)=>{
+    // console.log(btn)
+    btn.addEventListener("click",()=>{
+        if(btn.classList.contains("conter")){
+          countClick()
+          if (counter==1){
+            optionCount("Subhanallah")
+          }else if(counter==34){
+            optionCount("Alhamdulillah")
+
+          }else if(counter==67){
+            optionCount("Allahuakbar")
+        }
+
+
+        //reset button and number button
+
+        }else if ( btn.classList.contains("reset")){
+            input.value=reset
+            counter=0;
+           optionCount("")
+        }else{
+            input.value=number
+            counter=0;
+            optionCount("")
+        }
+           
+        
+    })
+})
+
+
+
+
+
+
